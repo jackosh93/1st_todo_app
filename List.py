157e42e5c1ceb34bd3738c9e1345933ffff1cc5b
flysharkey <jackosh36@gmail.com>
2023-02-15 14:49:53 -0500

    Initial commit

--- List.py
+++ List.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout='wide')
+
 def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
@@ -11,7 +13,13 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app!")
-st.write("The goal is to increase your productivity.")
+st.write("The goal is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="What's on the list?",
+              placeholder="Ambitions, passions, etc....",
+              on_change=add_todo, key="new_todo")
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -21,8 +29,4 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-
-st.text_input(label="What's on the list?",
-              placeholder="Ambitions, passions, etc....",
-              on_change=add_todo, key="new_todo")
 
---
+++ pages/About.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("Hello my name is Jack! "
+         "I'm trying to learn how to code.")
